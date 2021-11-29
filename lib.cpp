3296223fde1e6b89e2e68593e59becd01208a73c
S5219580 <91791844+S5219580@users.noreply.github.com>
2021-11-29 08:55:48 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,1 +1,12 @@
 
+#include "riconoscimento.h"
+void riconoscimento(char &a,bool &ris){
+    if (a<91&&a>64) {
+        ris=true;
+        a=a+32;}
+    else if(a<123&&a>96){
+        ris=true;
+        a=a-32;
+    }
+    else {ris=false;}
+}
