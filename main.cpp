6542e70b196d0ad622e762379bb15f52c066c839
S5219580 <91791844+S5219580@users.noreply.github.com>
2021-11-29 08:57:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
+#include "riconoscimento.h"
+using namespace std;
 
 int main(){
-  return 0;
+    char input=0;
+    bool ris=0;
+    cin>> input;
+    riconoscimento(input, ris);
+    if (ris==true){
+        cout<<"True"<<endl <<input;
+    }
+    else{cout<<"False";}
+    return 0;
 }
